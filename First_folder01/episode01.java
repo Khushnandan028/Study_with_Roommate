4835dc2378c2cd178d28027c0c4b48e9f7dec66b
Khushnandan Kumar <avadeshkumar7851@gmail.com>
2026-02-01 19:14:25 +0000

    first commit

---
+++ First_folder01/episode01.java
@@ -0,0 +1,7 @@
+class episode01{
+    public static void main(String args[]){
+        System.out.println("Hii , dear Roommates..");
+        System.out.println("We are Learning Java peogramming language.");
+
+    }
+}
---
+++ First_folder01/file01.jav
@@ -0,0 +1,7 @@
+class file01{
+    public static void main(){
+        Scanner sc= sc.next(System.in);
+        int
+        System.out.println("Good Evening..");
+    }
+}
