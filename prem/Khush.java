92f316c8dd554966b9051736cc40ded960fcf807
Khushnandan Kumar <avadeshkumar7851@gmail.com>
2026-02-02 02:20:40 +0530

    Happy

---
+++ prem/Khush.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ 
+ public class Khush {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println("My name is  :" +name);
+        System.out.println("Age is "+age);
+    }
+} 
